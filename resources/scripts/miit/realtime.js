74886e4c315ad9b505d2d9c66c32fd7b28fd6076
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-06-30 15:24:17 +0200

    Add reconnection system

--- resources/scripts/miit/realtime.js
+++ resources/scripts/miit/realtime.js
@@ -1,19 +1,44 @@
 (function(){
     var MiitRealtime = injector.resolve(function() {
         // Initiliaze primus            
-        var primus = Primus.connect();
+        var primus    = Primus.connect(),
+            connected = false,
+            retry     = 1;
 
-        // Bind incoming data
-        var onData = function(data) {
+        function reconnect() {
+            retry *= 1.3;
+
+            if(false === connected) {
+
+                // Retry after
+                setTimeout(
+                    primus.open.bind(primus),
+                    Math.round(1000 * retry)
+                );
+            }
+        }
+
+        // Listen all data
+        primus.on('data', function(data) {
             console.log(data);
 
             if(primus.reserved(data.event)) return;
 
             primus.emit.call(primus, data.event, data);
-        };
+        });
 
-        // Listen all data
-        primus.on('data', onData);
+        // Listen for connection
+        primus.on('open', function() {
+            retry     = 1;
+            connected = true;
+        });
+
+        // Listenen for end
+        primus.on('end', function closed() {
+            // Set as not connected and reconnect
+            connected = false;
+            reconnect();
+        });
 
         return {
             send: function(eventName, data) {
