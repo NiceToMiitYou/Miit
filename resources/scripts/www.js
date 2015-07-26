d4e201d1851f662a278e5a59c975412945662a19
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-07-26 02:48:50 +0200

    Fix the newsletter from contact and remove some comments

--- resources/scripts/www.js
+++ resources/scripts/www.js
@@ -9,5 +9,8 @@ var CreateTeam = require('templates/www/create-team.jsx'),
 
 global.onload = function(){
     //React.render(<CreateTeam />, document.getElementById('create-team'));
-    React.render(<NewsLetter />, document.getElementById('create-team'));
+    React.render(<NewsLetter variant={false} />, document.getElementById('create-team'));
+
+    // Keep contact field
+    React.render(<NewsLetter variant={true} />, document.getElementById('keep-contact'));
 };
