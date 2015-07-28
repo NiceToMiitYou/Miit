a5824ff9c11f0a15014960f56644594a18142518
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-07-28 03:36:35 +0200

    Add toSlug extension for strings

--- resources/scripts/lib/string-extensions.js
+++ resources/scripts/lib/string-extensions.js
@@ -9,3 +9,13 @@ String.prototype.capitalize = function(){
 String.prototype.dashToCapitalize = function(){
     return this.replace(/_/g, ' ').capitalize().replace(/ /g, '');
 };
+
+String.prototype.toSlug = function(){
+    return this.toLowerCase()
+                .replace(/\s+/g, '-')           // Replace spaces with -
+                .replace(/_+/g, '-')            // Replace dash with -
+                .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+                .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+                .replace(/^-+/, '')             // Trim - from start of text
+                .replace(/-+$/, '');            // Trim - from end of text
+};
