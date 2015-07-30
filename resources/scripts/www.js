f1fa0ffb674c0847be799ecbe789a857bcbf6c8c
Jordan Cortet <jordan.cortet@itevents.fr>
2015-07-30 04:09:52 +0200

    Fix create team design

--- resources/scripts/www.js
+++ resources/scripts/www.js
@@ -19,8 +19,8 @@ var CreateTeam = require('templates/create-team.jsx'),
 global.onload = function() {
     Bubbles('canvas');
 
-    //React.render(<CreateTeam />, document.getElementById('create-team'));
-    React.render(<NewsLetter variant={false} />, document.getElementById('create-team'));
+    React.render(<CreateTeam />, document.getElementById('create-team'));
+    // React.render(<NewsLetter variant={false} />, document.getElementById('create-team'));
 
     // Keep contact field
     React.render(<NewsLetter variant={true} />, document.getElementById('keep-contact'));
--- resources/scripts/www/templates/create-team.jsx
+++ resources/scripts/www/templates/create-team.jsx
@@ -98,21 +98,21 @@ var CreateTeam = React.createClass({
                 
                 <div className="row pt20 pb20">
                     <div className="col-md-6 mb10">
-                        <div className="input-field left-icon icon-transparent push0">
-                            <i className="fa fa-envelope-o"></i>
+                        <div className="input-field left-icon icon-transparent pt5 pb5 push0">
+                            <i className="fa fa-envelope-o pt5 pb5"></i>
                             <input type="text" className={classes_email} placeholder={this.props.placeholder.email} ref="email" />
                         </div>
                     </div>
 
                     <div className="col-md-6">
-                        <div className="input-field left-icon icon-transparent col5">
-                            <i className="fa fa-users"></i>
+                        <div className="input-field left-icon icon-transparent pt5 pb5 col5">
+                            <i className="fa fa-users pt5 pb5"></i>
                             <input type="text" className={classes_team}  placeholder={this.props.placeholder.team}  ref="team" />
                         </div>
                     </div>
                 </div>
 
-                <button type="submit" className="btn btn-dark mt5">{this.props.submit}</button>
+                <button type="submit" className="btn btn-info pl20 pr20 pt15 pb15">{this.props.submit}</button>
                 
             </form>
         );
