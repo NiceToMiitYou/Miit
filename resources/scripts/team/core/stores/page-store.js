c34c3daa5ca457d89999982ea97b07296522bf00
Jordan Cortet <jordan.cortet@itevents.fr>
2015-08-09 21:34:04 +0200

    Toggle menu CSS and open by default

--- resources/scripts/team/core/stores/page-store.js
+++ resources/scripts/team/core/stores/page-store.js
@@ -24,7 +24,7 @@ var notFoundPage = config['404'];
 var CurrentMainPage, CurrentApplicationPage, Argument;
 
 // Menu State
-var MenuOpened = false;
+var MenuOpened = true;
 
 // A storage for all pages
 var PageStorage = new DataStore('pages');
