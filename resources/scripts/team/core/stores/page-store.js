c6d4046c33df6edb8757277f7aa6f59c02f2ab9e
Jordan Cortet <jordan.cortet@itevents.fr>
2015-08-31 21:17:44 +0200

    Fix IE8 menu stat

--- resources/scripts/team/core/stores/page-store.js
+++ resources/scripts/team/core/stores/page-store.js
@@ -24,7 +24,7 @@ var notFoundPage = config['404'];
 var CurrentMainPage, CurrentApplicationPage, Argument;
 
 // Menu State
-var MenuOpened = window.innerWidth >= 768;
+var MenuOpened = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) >= 768;
 
 // A storage for all pages
 var PageStorage = new DataStore('pages');
