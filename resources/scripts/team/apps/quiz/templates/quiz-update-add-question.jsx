edb81a7e10b47dc2a825197f90b44fa997c09b08
Jordan Cortet <jordan.cortet@itevents.fr>
2015-09-01 17:46:37 +0200

    Update add-question design

--- resources/scripts/team/apps/quiz/templates/quiz-update-add-question.jsx
+++ resources/scripts/team/apps/quiz/templates/quiz-update-add-question.jsx
@@ -19,9 +19,12 @@ var QuizCreate = React.createClass({
     getDefaultProps: function () {
         return {
             text: {
-                open:     'Ouverte',
-                unique:   'Unique',
-                multiple: 'Multiple'
+                open:         'Ouverte',
+                openDesc:     'Créer une question ouverte avec une zone de text dans laquelle l\'utilisateur pourra y taper sa réponse',
+                unique:       'Unique',
+                uniqueDesc:   'Créer une question unique avec une liste de réponsse possibles parmis lesquelles l\'utilisateur ne pourra en choisir qu\'une seule',
+                multiple:     'Multiple',
+                multipleDesc: 'Créer une question multiple avec une liste de réponses possibles parmis lesquelles l\'utilisateur pourra en choisir une ou plusieurs'
             },
             questions: null
         };
@@ -45,9 +48,31 @@ var QuizCreate = React.createClass({
 
         return (
             <div className="miit-component quiz-update-add-question">
-                <div onClick={this.onClick.bind(this, 1)}>{this.props.text.unique}</div>
-                <div onClick={this.onClick.bind(this, 2)}>{this.props.text.multiple}</div>
-                <div onClick={this.onClick.bind(this, 3)}>{this.props.text.open}</div>
+
+                <div className="add-question t1" onClick={this.onClick.bind(this, 1)}>
+                    <i className="fa fa-check-circle-o bg-blue-green darken-5"></i> 
+                    <div className="add-question-content">
+                        <h3>{this.props.text.unique}</h3>
+                        <p>{this.props.text.uniqueDesc}</p>
+                    </div>
+                </div>
+
+                <div className="add-question t2" onClick={this.onClick.bind(this, 2)}>
+                    <i className="fa fa-check-square-o"></i>
+                    <div className="add-question-content">
+                        <h3>{this.props.text.multiple}</h3>
+                        <p>{this.props.text.multipleDesc}</p>
+                    </div>
+                </div>
+
+                <div className="add-question t3" onClick={this.onClick.bind(this, 3)}>
+                    <i className="fa fa-keyboard-o bg-purple"></i>
+                    <div className="add-question-content">
+                        <h3>{this.props.text.open}</h3>
+                        <p>{this.props.text.openDesc}</p>
+                    </div>
+                </div>
+
             </div>
         );
     }
