d84254ecd8d470ff2e4ceffc1097ace891932912
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-09-09 14:30:45 +0200

    Extract interval from chat to date format.

--- resources/scripts/common/templates/date-format.jsx
+++ resources/scripts/common/templates/date-format.jsx
@@ -7,12 +7,28 @@ var Moment = require('moment');
 Moment.locale('fr');
 
 var DateFormat = React.createClass({
+    IntervalId: null,
+
     getDefaultProps: function () {
         return {
             date:   new Date(),
             format: 'llll',
             from:   false
         };
+    },
+
+    componentDidMount: function () {
+        if(true === this.props.from) {
+            // Refresh the list for date update
+            this.IntervalId = setInterval(this.forceUpdate.bind(this), 15000);
+        }
+    },
+
+    componentWillUnmount: function () {
+        if(null !== IntervalId) {
+            // Clear refresh of the date
+            clearInterval(this.IntervalId);
+        }
     },
 
     formatDate: function() {
