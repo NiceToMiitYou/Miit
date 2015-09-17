76d39c50b7c1e9aadb6bd6eb88fc51cdbfbf1e26
Jordan Cortet <jordan.cortet@itevents.fr>
2015-09-17 16:38:01 +0200

    Update welcome page

--- resources/scripts/team/pages/welcome.jsx
+++ resources/scripts/team/pages/welcome.jsx
@@ -7,7 +7,8 @@ var PageStore = require('core/stores/page-store');
 var Layout = require('./layouts/default-layout.jsx');
 
 // Include components
-var Link = require('core/templates/components/link.jsx');
+var Link            = require('core/templates/components/link.jsx'),
+    AppStoreList    = require('core/templates/app-store/app-store-list.jsx');
 
 var WelcomePage = React.createClass({
     getDefaultProps: function () {
@@ -32,15 +33,15 @@ var WelcomePage = React.createClass({
                         Découvrez les nombreuses posssibilités de pérsonnalisation qui s{"'"}offrent à vous.
                     </h3>
 
-                    <p className="mt20">
+                    <p className="mt40">
                         Pour commencer rapidement à communiquer vous pouvez choisir un modèle de Miit.
                     </p>
-                    <div className="row mt20">
+                    <div className="row mt20 templates">
                         <div className="col-md-4 col-lg-4 mb20">
                             <div className="panel small">
                                 <h2 className="panel-title">Conférences</h2>
                                 <div className="panel-content">
-                                    <p></p>
+                                    <p className="mb20">Construisez un espace de communication pour les conférences avec un chat, des questionnaires et mur de questions</p>
                                     <span>Applications:</span>
                                     <div>
                                         <span className="miit-app-icon">
@@ -54,13 +55,16 @@ var WelcomePage = React.createClass({
                                         </span>
                                     </div>
                                 </div>
+                                <div className="panel-footer">
+                                    <i className="fa fa-plus mr10"></i>Ajouter les applications
+                                </div>
                             </div>
                         </div>
                         <div className="col-md-4 col-lg-4 mb20">
                             <div className="panel small">
                                 <h2 className="panel-title">Réunion</h2>
                                 <div className="panel-content">
-                                    <p></p>
+                                    <p className="mb20">Construisez un espace pour simplifier vos réunions grace à un chat, un partage de documents et un mur de questions</p>
                                     <span>Applications:</span>
                                     <div>
                                         <span className="miit-app-icon">
@@ -74,13 +78,16 @@ var WelcomePage = React.createClass({
                                         </span>
                                     </div>
                                 </div>
+                                <div className="panel-footer">
+                                    <i className="fa fa-plus mr10"></i>Ajouter les applications
+                                </div>
                             </div>
                         </div>
                         <div className="col-md-4 col-lg-4 mb20">
                             <div className="panel small">
                                 <h2 className="panel-title">Equipe</h2>
                                 <div className="panel-content">
-                                    <p></p>
+                                    <p className="mb20">Gardez le contact et echanger facilement avec votre/vos equipe(s) avec un simple chat et un partage de documents</p>
                                     <span>Applications:</span>
                                     <div>
                                         <span className="miit-app-icon">
@@ -91,13 +98,18 @@ var WelcomePage = React.createClass({
                                         </span>
                                     </div>
                                 </div>
+                                <div className="panel-footer">
+                                    <i className="fa fa-plus mr10"></i>Ajouter les applications
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <p>
-                        <b>Ou</b> créer un éspace pesonnalisé en ajoutant vous même des applications.
+                    <p className="mb20 mt20">
+                        <b>Ou</b> créer un espace pesonnalisé en ajoutant vous même des applications.
                     </p>
+
+                    <AppStoreList />
                 </div>
             </Layout>
         );
